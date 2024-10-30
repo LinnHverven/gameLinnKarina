7cf3ee2ee073df465c9db38a1f911ed5431c654d
LinnHverven <linn.hverven@gmail.com>
2024-10-30 09:41:01 +0100

    Första

---
+++ src/gameLinnKarina.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class gameLinnKarina extends JFrame implements ActionListener {
+    JButton button = new JButton();
+    JPanel panel = new JPanel();
+
+    //Kommentera ut ett spel som vinner för att visa att det går. Kör programmet två gånger.
+    public gameLinnKarina() {
+        setVisible(true);
+        setTitle("Game");
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        new gameLinnKarina();
+    }
+}
